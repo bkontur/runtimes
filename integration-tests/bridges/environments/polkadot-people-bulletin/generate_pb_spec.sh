0c698300b65c1c51c2e63525152b7d2da39ebf09
Anthony Kveder <anthony.kveder@parity.io>
2025-06-03 13:06:30 +0100

    Fixed chain spec builder call

--- integration-tests/bridges/environments/polkadot-people-bulletin/generate_pb_spec.sh
+++ integration-tests/bridges/environments/polkadot-people-bulletin/generate_pb_spec.sh
@@ -7,8 +7,8 @@ shift
 # Add Alice as bridge owner
 # We do this only if there is a `.genesis.runtimeGenesis.patch` object.
 # Otherwise we're working with the raw chain spec.
-$POLKADOT_BULLETIN_BINARY_PATH -- build-spec --chain=local > sc_init.json
-$POLKADOT_BULLETIN_BINARY_PATH -- build-spec --chain=sc_init.json | jq 'if .genesis.runtimeGenesis.patch
+$POLKADOT_BULLETIN_BINARY_PATH build-spec --chain=local > sc_init.json
+$POLKADOT_BULLETIN_BINARY_PATH build-spec --chain=sc_init.json | jq 'if .genesis.runtimeGenesis.patch
     then .genesis.runtimeGenesis.patch.bridge'$bridged_chain'Grandpa.owner = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
     else .
     end'
