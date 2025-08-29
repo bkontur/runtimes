f8081f299a0369389f9762e7ee688b9bb2a2bb21
Branislav Kontur <bkontur@gmail.com>
2025-08-29 23:55:31 +0200

    More logs

--- integration-tests/bridges/environments/polkadot-people-bulletin/helper.sh
+++ integration-tests/bridges/environments/polkadot-people-bulletin/helper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-$ENV_PATH/bridges_people_bulletin.sh "$@"
+if [ $1 == "auto-log" ]; then
+    shift # ignore "auto-log"
+    log_name=$1
+    $ENV_PATH/bridges_people_bulletin.sh "$@" >$TEST_DIR/logs/$log_name.log
+else
+    $ENV_PATH/bridges_people_bulletin.sh "$@"
+fi
